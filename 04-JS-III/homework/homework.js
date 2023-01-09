58141b02eef01657f6033c098f980ddd06e92e29
ragh3009 <soporteroger30@gmail.com>
2023-01-09 13:09:33 -0500

    js III corregido

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -38,8 +38,9 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
-  array[array.length] = elemento;
-  return array;
+  array.push(elemento)
+
+  return array
 }
 
 
